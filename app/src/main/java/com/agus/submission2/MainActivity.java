6fb0c0f1b9cf108796fbcbffca7d70b436fa4c4d
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-01-09 23:47:36 +0700

    add fragment & tab layout

--- app/src/main/java/com/agus/submission2/MainActivity.java
+++ app/src/main/java/com/agus/submission2/MainActivity.java
@@ -1,8 +1,17 @@
 package com.agus.submission2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.agus.submission2.adapter.SectionsPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -10,5 +19,29 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		SectionsPagerAdapter sectionsPageAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+		ViewPager viewPager = findViewById(R.id.view_pager);
+		viewPager.setAdapter(sectionsPageAdapter);
+		TabLayout tabs = findViewById(R.id.tabs);
+		tabs.setupWithViewPager(viewPager);
+		
+		getSupportActionBar().setElevation(0);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_data, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if (item.getItemId() == R.id.menu_change_language) {
+			Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+			startActivity(mIntent);
+		}
+		
+		return super.onOptionsItemSelected(item);
 	}
 }
---
+++ app/src/main/java/com/agus/submission2/adapter/SectionsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.agus.submission2.adapter;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.agus.submission2.R;
+import com.agus.submission2.fragment.FilmFragment;
+import com.agus.submission2.fragment.TVFragment;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	private final Context mContext;
+	
+	
+	public SectionsPagerAdapter(Context context, FragmentManager fm) {
+		super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+		mContext = context;
+	}
+	
+	@StringRes
+	private final int[] TAB_TITLES = new int [] {
+		R.string.tab_text_1,
+		R.string.tab_text_2
+	};
+	
+	@NonNull
+	@Override
+	public Fragment getItem(int position) {
+		Fragment fragment = null;
+		
+		switch (position) {
+			case 0:
+				fragment = new FilmFragment();
+				break;
+			
+			case 1:
+				fragment = new TVFragment();
+				break;
+		}
+		
+		return fragment;
+	}
+	
+	@Override
+	public int getCount() {
+		return 2;
+	}
+	
+	@Nullable
+	@Override
+	public CharSequence getPageTitle(int position) {
+		return mContext.getResources().getString(TAB_TITLES[position]);
+	}
+}
